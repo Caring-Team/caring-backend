585536ea0a97266604fd385c54c093564aa91ed5
나의찬 <cab07036@naver.com>
2025-09-15 20:30:21 +0900

    Feat: 초기 spring Boot 세팅

---
+++ src/main/java/com/caring/caringbackend/CaringBackendApplication.java
@@ -0,0 +1,13 @@
+package com.caring.caringbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaringBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaringBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/caring/caringbackend/CaringBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caring.caringbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaringBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
