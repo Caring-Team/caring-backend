22ec28d6dd9d88e1db0843bc4222f21b72414465
SongTaeKwon <taekwon0321@gmail.com>
2025-10-27 21:26:13 +0900

    feat(auth): OAuth2 로그인 용 DTO 및 Properties 추가

---
+++ src/main/java/com/caring/caringbackend/domain/auth/properties/OAuth2ProviderProperties.java
@@ -0,0 +1,26 @@
+package com.caring.caringbackend.domain.auth.properties;
+
+import java.util.Map;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Getter
+@Setter
+@Configuration
+@ConfigurationProperties(prefix = "oauth2")
+public class OAuth2ProviderProperties {
+
+    private Map<String, ProviderProperties> providers;
+
+    @Getter
+    @Setter
+    public static class ProviderProperties {
+        private String clientId;
+        private String clientSecret;
+        private String redirectUri;
+        private String tokenUri;
+        private String userInfoUri;
+    }
+}
