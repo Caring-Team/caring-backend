15143a9ca4826e0559100f88b9e2255b73cda4e4
SongTaeKwon <taekwon0321@gmail.com>
2025-10-27 20:53:35 +0900

    feat(auth): WebClient Bean 등록

---
+++ src/main/java/com/caring/caringbackend/global/config/webclient/WebClientConfig.java
@@ -0,0 +1,19 @@
+package com.caring.caringbackend.global.config.webclient;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Profile("dev")
+@Configuration
+public class WebClientConfig {
+    @Bean
+    public WebClient webClient(WebClient.Builder builder) {
+        return builder
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .build();
+    }
+}
