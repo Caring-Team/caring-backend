9caa4eddc075dedc196418e69821087315767b5e
SongTaeKwon <taekwon0321@gmail.com>
2025-10-27 20:46:57 +0900

    refactor(auth): CredentialType key값 추가

--- src/main/java/com/caring/caringbackend/domain/user/guardian/entity/CredentialType.java
+++ src/main/java/com/caring/caringbackend/domain/user/guardian/entity/CredentialType.java
@@ -1,8 +1,25 @@
 package com.caring.caringbackend.domain.user.guardian.entity;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum CredentialType {
-    LOCAL,
-    OAUTH_KAKAO,
-    OAUTH_NAVER,
-    OAUTH_GOOGLE
+    LOCAL("local"),
+    OAUTH_KAKAO("kakao"),
+    OAUTH_NAVER("naver"),
+    OAUTH_GOOGLE("google"),
+    UNKNOWN("unknown");
+
+    private final String key;
+
+    public static CredentialType fromKey(String key) {
+        for (CredentialType type : CredentialType.values()) {
+            if (type.key.equals(key)) {
+                return type;
+            }
+        }
+        return UNKNOWN;
+    }
 }
