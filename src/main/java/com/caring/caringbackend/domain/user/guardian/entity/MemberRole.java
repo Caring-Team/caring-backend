d9e9a9bbb8e13c3f19bec3681ff3a0357cc12cf7
SongTaeKwon <taekwon0321@gmail.com>
2025-10-27 21:02:33 +0900

    feat(auth): MemberRole Key값 추가

--- src/main/java/com/caring/caringbackend/domain/user/guardian/entity/MemberRole.java
+++ src/main/java/com/caring/caringbackend/domain/user/guardian/entity/MemberRole.java
@@ -1,6 +1,16 @@
 package com.caring.caringbackend.domain.user.guardian.entity;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum MemberRole {
-    TEMP_USER,
-    USER
+    TEMP_OAUTH("ROLE_TEMP_OAUTH", "임시 사용자"),
+    TEMP_LOCAL("ROLE_TEMP_LOCAL", "임시 사용자"),
+    USER("ROLE_USER", "사용자");
+
+    private final String key;
+    private final String tittle;
+
 }
