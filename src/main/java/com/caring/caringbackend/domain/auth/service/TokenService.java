7a87e3bcd017d58b0521f9acd139aa39371490cf
SongTaeKwon <taekwon0321@gmail.com>
2025-10-28 20:24:13 +0900

    refactor(auth): 사용하지 않는 의존성 삭제

--- src/main/java/com/caring/caringbackend/domain/auth/service/TokenService.java
+++ src/main/java/com/caring/caringbackend/domain/auth/service/TokenService.java
@@ -7,10 +7,6 @@ import com.caring.caringbackend.domain.auth.dto.request.TokenRefreshRequest;
 import com.caring.caringbackend.domain.auth.dto.response.JwtTokenResponse;
 import com.caring.caringbackend.domain.auth.entity.RefreshToken;
 import com.caring.caringbackend.domain.auth.repository.RefreshTokenRepository;
-import com.caring.caringbackend.domain.institution.profile.entity.InstitutionAdmin;
-import com.caring.caringbackend.domain.institution.profile.repository.InstitutionAdminRepository;
-import com.caring.caringbackend.domain.user.guardian.entity.Member;
-import com.caring.caringbackend.domain.user.guardian.repository.MemberRepository;
 import com.caring.caringbackend.global.exception.BusinessException;
 import com.caring.caringbackend.global.exception.ErrorCode;
 import com.caring.caringbackend.global.security.JwtUtils;
@@ -23,8 +19,6 @@ import org.springframework.transaction.annotation.Transactional;
 public class TokenService {
     private final RefreshTokenRepository refreshTokenRepository;
     private final JwtUtils jwtUtils;
-    private final MemberRepository memberRepository;
-    private final InstitutionAdminRepository institutionAdminRepository;
 
     @Transactional
     public JwtTokenResponse generateToken(GenerateTokenDto generateTokenDto) {
