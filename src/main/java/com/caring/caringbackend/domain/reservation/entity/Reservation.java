3d96b2b7b8bbde0853a9d38383abd593d9695cf1
나의찬 <cab07036@naver.com>
2025-11-09 02:10:00 +0900

    refactor(Reservation): 예약 엔티티 수정
    기존에는 InstitutionCounel에 대한 데이터를 참조하고 있었습니다. 하지만 InstitutionCounselDetail 데이터를 참조하면 중복된 데이터 (예약 이름, 설명, 예약 날짜)를 중복 제거할 수 있어서 InstitutionCounselDetail 데이터로 변경하였고 중복된 컬럼은 제거하였습니다.
    
    또한 예약 시간에 대한 자료형이 Stinrg -> LocalTime으로 변경하였습니다.

--- src/main/java/com/caring/caringbackend/domain/reservation/entity/Reservation.java
+++ src/main/java/com/caring/caringbackend/domain/reservation/entity/Reservation.java
@@ -1,6 +1,7 @@
 package com.caring.caringbackend.domain.reservation.entity;
 
 import com.caring.caringbackend.domain.institution.counsel.entity.InstitutionCounsel;
+import com.caring.caringbackend.domain.institution.counsel.entity.InstitutionCounselDetail;
 import com.caring.caringbackend.domain.user.elderly.entity.ElderlyProfile;
 import com.caring.caringbackend.domain.user.guardian.entity.Member;
 import com.caring.caringbackend.global.model.BaseTimeEntity;
@@ -11,6 +12,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
+import java.time.LocalTime;
 
 /**
  * 예약 엔티티
@@ -27,26 +29,26 @@ public class Reservation extends BaseTimeEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    // 예약 이름
-    @Column(nullable = false, length = 100)
-    private String title;
-
-    // 예약 설명
-    @Column(length = 500)
-    private String description;
-
-    // 예약 날짜
-    @Column(nullable = false)
-    private LocalDate reservationDate;
+//    // 예약 이름
+//    @Column(nullable = false, length = 100)
+//    private String title;
+//
+//    // 예약 설명
+//    @Column(length = 500)
+//    private String description;
+//
+//    // 예약 날짜
+//    @Column(nullable = false)
+//    private LocalDate reservationDate;
 
     // 예약 시간
     @Column(nullable = false)
-    private String reservationTime;
+    private LocalTime reservationTime;
 
-    // 기관 상담 서비스
+    // 기관 상담 서비스 디테일
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "institution_counsel_id", nullable = false)
-    private InstitutionCounsel institutionCounsel;
+    @JoinColumn(name = "institution_counsel_Detail_id", nullable = false)
+    private InstitutionCounselDetail counselDetail;
 
     // Member
     @ManyToOne(fetch = FetchType.LAZY)
@@ -61,15 +63,26 @@ public class Reservation extends BaseTimeEntity {
     // 예약 상태
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
-    private ReservationStatus status;
+    private ReservationStatus status = ReservationStatus.COMPLETED;
 
     @Builder
-    public Reservation(InstitutionCounsel institutionCounsel, Member member, ElderlyProfile elderlyProfile, ReservationStatus status) {
-        this.institutionCounsel = institutionCounsel;
+    public Reservation(InstitutionCounselDetail counselDetail,
+                       Member member,
+                       ElderlyProfile elderlyProfile,
+                       LocalTime reservationTime
+    ) {
+        this.counselDetail = counselDetail;
+        this.reservationTime = reservationTime;
         this.member = member;
         this.elderlyProfile = elderlyProfile;
-        this.status = status;
     }
 
-    // TODO: 필요한 도메인 로직 작성
+    public static Reservation createReservation(InstitutionCounselDetail counselDetail, Member member, ElderlyProfile elderlyProfile, LocalTime reservationTime) {
+        return Reservation.builder()
+                .counselDetail(counselDetail)
+                .member(member)
+                .elderlyProfile(elderlyProfile)
+                .reservationTime(reservationTime)
+                .build();
+    }
 }
