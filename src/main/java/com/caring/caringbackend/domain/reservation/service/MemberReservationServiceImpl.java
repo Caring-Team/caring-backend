3e2693fd316c39e4958ca7d00f18aeb4bf36d40c
나의찬 <cab07036@naver.com>
2025-11-15 01:45:25 +0900

    fix(Reservation): 예외 처리 및 오류 처리
    동시 예약 예외 처리와 파라미터 잘못 전달 오류를 수정하였습니다.

--- src/main/java/com/caring/caringbackend/domain/reservation/service/MemberReservationServiceImpl.java
+++ src/main/java/com/caring/caringbackend/domain/reservation/service/MemberReservationServiceImpl.java
@@ -12,8 +12,11 @@ import com.caring.caringbackend.domain.user.guardian.repository.MemberRepository
 import com.caring.caringbackend.global.exception.BusinessException;
 import com.caring.caringbackend.global.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
+import org.springframework.orm.ObjectOptimisticLockingFailureException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import static com.caring.caringbackend.global.exception.ErrorCode.RESERVATION_TIME_NOT_AVAILABLE;
 
 @Service
 @Transactional
@@ -28,36 +31,40 @@ public class MemberReservationServiceImpl implements MemberReservationService {
 
     @Override
     public void createMemberReservation(Long memberId, MemberReservationCreateRequestDto requestDto) {
-        // 기관 상담 ID, 날짜, 예약 시간, erderlyProfile,
-        Member member = getMember(memberId);
-        ElderlyProfile elderlyProfile = getElderlyProfile(memberId, requestDto);
-        InstitutionCounselDetail counselDetail = getCounselDetail(requestDto);
+        try {
+            // 기관 상담 ID, 날짜, 예약 시간, erderlyProfile,
+            Member member = getMember(memberId);
+            ElderlyProfile elderlyProfile = getElderlyProfile(memberId, requestDto);
+            InstitutionCounselDetail counselDetail = getCounselDetail(requestDto);
 
-        // 예약이 가능한지 확인
-        validateIsAvailable(requestDto, counselDetail);
+            // 예약이 가능한지 확인
+            validateIsAvailable(requestDto, counselDetail);
 
-        // 예약 생성
-        Reservation reservation = Reservation.createReservation(
-                counselDetail,
-                member,
-                elderlyProfile,
-                requestDto.getStartTime());
+            // 예약 생성
+            Reservation reservation = Reservation.createReservation(
+                    counselDetail,
+                    member,
+                    elderlyProfile,
+                    requestDto.getStartTime());
 
-        // 예약 시간대 비트마스크 업데이트
-        counselDetail.markSlotAsReserved(requestDto.getSlotIndex());
+            // 예약 시간대 비트마스크 업데이트
+            counselDetail.markSlotAsReserved(requestDto.getSlotIndex());
 
-        // 예약 저장
-        reservationRepository.save(reservation);
+            // 예약 저장
+            reservationRepository.save(reservation);
+        } catch (ObjectOptimisticLockingFailureException e) {
+            throw new BusinessException(RESERVATION_TIME_NOT_AVAILABLE);
+        }
     }
 
     private ElderlyProfile getElderlyProfile(Long memberId, MemberReservationCreateRequestDto requestDto) {
-        return elderlyProfileRepository.findByIdAndMemberIdAndDeletedFalse(memberId, requestDto.getElderlyProfileId())
+        return elderlyProfileRepository.findByIdAndMemberIdAndDeletedFalse(requestDto.getElderlyProfileId(), memberId)
                 .orElseThrow(() -> new BusinessException(ErrorCode.ELDERLY_PROFILE_NOT_FOUND));
     }
 
     private static void validateIsAvailable(MemberReservationCreateRequestDto requestDto, InstitutionCounselDetail counselDetail) {
         if (!counselDetail.isSlotAvailable(requestDto.getSlotIndex())) {
-            throw new BusinessException(ErrorCode.RESERVATION_TIME_NOT_AVAILABLE);
+            throw new BusinessException(RESERVATION_TIME_NOT_AVAILABLE);
         }
     }
 
