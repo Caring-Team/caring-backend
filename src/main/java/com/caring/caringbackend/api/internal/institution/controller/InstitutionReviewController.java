d3b1031b876da305c77b3788e365dba14cc19954
나의찬 <cab07036@naver.com>
2025-11-30 02:13:17 +0900

    feat: 기관 리뷰 controller class 생성

---
+++ src/main/java/com/caring/caringbackend/api/internal/institution/controller/InstitutionReviewController.java
@@ -0,0 +1,7 @@
+package com.caring.caringbackend.api.internal.institution.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class InstitutionReviewController {
+}
